25f2aad1ee1ae474330de296ed096c404a509424
k33m0l <k33m0l@users.noreply.github.com>
2025-06-13 22:29:09 +0200

    Add wallpapers

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo 'Fetch Hyprland dotfiles...'
-cp -v $HOME/.config/hypr/hyprland.conf ./hypr/hyprland.conf
+cp -vr $HOME/.config/hypr/* ./hypr
 
 echo 'Fetch Waybar dotfiles...'
 cp -v -r $HOME/.config/waybar/* ./waybar
 
+echo 'Fetch Wallpapers...'
+cp -vr $HOME/Pictures/Wallpaper/* ./Pictures/Wallpaper
