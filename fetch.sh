a9135a444bce32eea097e468449b7e6687650369
k33m0l <k33m0l@users.noreply.github.com>
2025-06-13 23:02:05 +0200

    zsh config

--- fetch.sh
+++ fetch.sh
@@ -8,3 +8,7 @@ cp -v -r $HOME/.config/waybar/* ./waybar
 
 echo 'Fetch Wallpapers...'
 cp -vr $HOME/Pictures/Wallpaper/* ./Pictures/Wallpaper
+
+echo 'Fetch zsh config...'
+cp -v $HOME/.zshrc ./zsh/.zshrc
+
