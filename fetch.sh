3ad18ac0b6cc088c30d7adde6a3569a2b6e02d75
k33m0l <k33m0l@users.noreply.github.com>
2025-06-22 18:15:14 +0200

    fix wallpaper location

--- fetch.sh
+++ fetch.sh
@@ -7,7 +7,7 @@ echo 'Fetch Waybar dotfiles...'
 cp -v -r $HOME/.config/waybar/* ./waybar
 
 echo 'Fetch Wallpapers...'
-cp -vr $HOME/Pictures/Wallpaper/* ./Pictures/Wallpaper
+cp -vr $HOME/Documents/Wallpapers/* ./Wallpapers
 
 echo 'Fetch zsh config...'
 cp -v $HOME/.zshrc ./zsh/.zshrc
